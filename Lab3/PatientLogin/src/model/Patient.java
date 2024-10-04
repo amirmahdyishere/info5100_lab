c7733c8f261a863beb97d598e5984bc0c909180d
Amirmahdy <pourvahabyanbary.a@northeastern.edu>
2024-10-03 20:51:36 -0400

    Patient Registeration Application on JAVA swing

---
+++ Lab3/PatientLogin/src/model/Patient.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author amirp
+ */
+public class Patient {
+    
+    //Datas:
+    private String fname;
+    private String lname;
+    private String gender;
+    private String patient_type;
+    private String age;
+    private String Phone;
+    private String email;
+
+    //Methods:
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPatient_type() {
+        return patient_type;
+    }
+
+    public void setPatient_type(String patient_type) {
+        this.patient_type = patient_type;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ImageIcon getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(ImageIcon profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+    private ImageIcon profilePicture;
+    
+}
